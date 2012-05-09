6b1cebf0f974525c5c50150fa060d008a6bb948d
Ian Beck <ian@onecrayon.com>
2012-05-09 09:05:05 -0700

    Added global page and basic skeleton logic for intercepting Amazon affiliate links

---
+++ global.js
@@ -0,0 +1,37 @@
+/**
+ * global.js
+ * 
+ * Provides the core logic for intercepting Amazon Affiliate links
+ */
+
+// Adds listener event for the tab to intercept Amazon affiliate loading
+var setupTabListeners = function(event) {
+	var tab = event.target;
+	// Ensure we are working with a tab, not a window (only windows have a tabs array property)
+	if (typeof tab.tabs === 'undefined') {
+		tab.addEventListener("beforeNavigate", interceptAffiliateLinks, false);
+	}
+};
+
+// Remove listener when tab closes
+var cleanupTabListeners = function(event) {
+	var tab = event.target;
+	// Ensure that we are working with a tab, not a window
+	if (typeof tab.tabs === 'undefined') {
+		tab.removeEventListener("beforeNavigate", interceptAffiliateLinks, false);
+	}
+};
+
+// Checks to see if we are visiting an affiliate link; the core of our extension
+var affRE = /^http:\/\/(?:|.+?\.)amazon\..+?(?:\?|&)tag=([a-z0-9_-]+).*?$/i;
+var interceptAffiliateLinks = function(event) {
+	var results = affRE.exec(event.url);
+	if (results !== null) {
+		// We have an affiliate link; kill the load!
+		event.preventDefault();
+	}
+};
+
+// Start listening to our open and close events
+safari.application.addEventListener("open", setupTabListeners, true);
+safari.application.addEventListener("close", cleanupTabListeners, true);
